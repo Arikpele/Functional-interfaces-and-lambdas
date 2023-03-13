02f3233b62fdc7aa3370e174cc1cac4a2475ddb4
Arikpele <79112835522@y.ru>
2023-03-13 18:20:24 +0300

    задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
+        Supplier<Integer> number = new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return (int) (Math.random() * 1000);
+            }
+        };
+        Supplier<Integer> number2 = () -> (int) (Math.random() * 1000);
 
     }
 }
