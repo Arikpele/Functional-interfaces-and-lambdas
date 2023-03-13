10be4879d8606aefe76aadb1ce12f94d93235c35
Arikpele <79112835522@y.ru>
2023-03-13 17:17:19 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
+        Predicate<Integer> predicate1 = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                if (integer < 0) {
+                    return false;
+                }
+                return true;
+            }
+        };
+        Predicate<Integer> predicate2 = x -> x > 0;
 
+        System.out.println(predicate2.test(20));
+        System.out.println(predicate1.test(-11));
     }
 }
